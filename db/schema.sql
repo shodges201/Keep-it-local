d5ca1ba2a86845a002226299b149f518319e9919
Backslash <lanceadrian13@gmail.com>
2019-08-09 11:29:51 -0400

    added dummy info

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE events_db;
+
 USE events_db;
 
 CREATE TABLE events
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int(20) NOT NULL AUTO_INCREMENT,
 	location varchar(255) NOT NULL,
     name varchar(255) NOT NULL,
 	category varchar(255) NOT NULL,
-	authorid int NOT NULL,
-	author varchar(255) NOT NULL,
-	upvotes int NOT NULL,
+	creator_id int(20) NOT NULL, AUTO_INCREMENT,
+	upvotes int(300) NOT NULL,
 	PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO events (location, name, category, upvotes) 
+VALUES ('Sommerset', 'David Blaine Magic Show', 'Magic', 5),
+ ('Atlantic City', 'Louis C.K', 'Comedy', 0), ('Newark', 'Alpha Phi Deltas Dage', 'Party', 2),
+('New York City', 'Kendrick Lamar', 'Concert', 10);
+
+

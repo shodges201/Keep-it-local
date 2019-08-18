b44a7effd2a2a82b08e50283841791db50234d5f
Scott Hodges <shodges201@gmail.com>
2019-08-18 17:31:32 -0400

    fixed issue with updated time/date being a moment object instead of string when updated in the table

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -346,7 +346,7 @@ module.exports = function (app) {
     }).then(function (resp) {
       console.log("code created");
       console.log(resp);
-      let now = moment(momentToString(moment().format()));
+      let now = momentToString(moment().format());
       console.log(now);
       db.User.update({ lastReferral: now }, { where: { userName: req.user.userName } }).then(function (data) {
         res.json(resp);
